f6f9b9c5ffecf6155cd267680774790003551b08
Joe Suk <ybjosuk@gmail.com>
2023-04-07 19:51:53 -0400

    gitignore

---
+++ themes/test.h
@@ -0,0 +1,19 @@
+/* static const char black[]       = "#1E1D2D";
+static const char gray2[]       = "#282737"; // unfocused window border
+static const char gray3[]       = "#585767";
+static const char gray4[]       = "#282737";
+static const char blue[]        = "#96CDFB";  // focused window border
+static const char green[]       = "#ABE9B3";
+static const char red[]         = "#F28FAD";
+static const char orange[]      = "#F8BD96";
+static const char yellow[]      = "#FAE3B0";
+static const char pink[]        = "#d5aeea";
+static const char col_borderbar[]  = "#1E1D2D"; // inner border
++static const char col_base00[]      = "#1e1e3f";
++static const char col_base01[]      = "#43d426";
++static const char col_base02[]      = "#f1d000";
++static const char col_base04[]      = "#6871ff";
++static const char col_base0C[]      = "#00c5c7";
++static const char col_base0D[]      = "#6943ff";
+*/
+
